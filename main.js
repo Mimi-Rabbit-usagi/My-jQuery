7cb246800df19e1619f491b6f2c851de5918791d
RabbitMimi <kitto.ringo@gmail.com>
2024-12-27 23:51:27 +0900

    複数の要素にCSSの変化をつける

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 
 $(() => {
   $("button").click(() => {
-    $("p").text("おはよう！");
+    // $("p").text("おはよう！");
+    // $("p").addClass("red-text");
+    $("p").text("おはよう！").addClass("red-text"); //メソッドをつなげることをメソッドチェーンと言う
+
+    // $("li").eq(2).addClass("red-text");
+    $("li:nth-child(odd),li:last-child").addClass("red-text"); //奇数番目と最後の要素の色を赤くする
   });
 });
