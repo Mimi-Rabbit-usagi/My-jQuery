df917ef4d906eb33491c407329b920090ec9fb6d
RabbitMimi <kitto.ringo@gmail.com>
2024-12-28 00:11:05 +0900

    要素の追加とinputタグ入力の反映

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 "use strict";
 
 $(() => {
+  const $input = $("input");
+  $input.focus();
   $("button").click(() => {
     // $("p").text("おはよう！");
     // $("p").addClass("red-text");
-    $("p").text("おはよう！").addClass("red-text"); //メソッドをつなげることをメソッドチェーンと言う
+    // $("p").text("おはよう！").addClass("red-text"); //メソッドをつなげることをメソッドチェーンと言う
 
     // $("li").eq(2).addClass("red-text");
-    $("li:nth-child(odd),li:last-child").addClass("red-text"); //奇数番目と最後の要素の色を赤くする
+    // $("li:nth-child(odd),li:last-child").addClass("red-text"); //奇数番目と最後の要素の色を赤くする
+    // $("<li>").text("New Item").appendTo("ul"); //新しいリストの追加　createElement()
+
+    $("<li>").text($input.val()).appendTo("ul"); //新しいリストの追加　createElement()
+    $input.val("").focus();
   });
 });
